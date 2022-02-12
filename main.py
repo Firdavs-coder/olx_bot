7ad69324a451331cd12ffcbea00607d84ceb8bb6
Firdavs-coder <shodiyev-f@mail.ru>
2022-02-12 21:37:28 +0500

    -

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 from requests.api import get
 import telebot
 from telebot import types
+import random
 
-# bot = telebot.TeleBot("5042865364:AAH4Tndi4n_vawDY9sXyEUlS4omPIePaVT4", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
-bot = telebot.TeleBot("2143029358:AAEUMfcLE7mUigh5NRrzJbWc75MGtLaPySk", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+bot = telebot.TeleBot("5042865364:AAH4Tndi4n_vawDY9sXyEUlS4omPIePaVT4", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+# bot = telebot.TeleBot("2143029358:AAEUMfcLE7mUigh5NRrzJbWc75MGtLaPySk", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
 # 0️⃣1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣🔟
 numbers = {
     '0':'0️⃣',
@@ -29,19 +30,19 @@ def start(message):
         subs = res.json()['subscribes_count']
         bot.send_message(message.chat.id, f"Obunachilaringiz <b>{subs}</b> ta", parse_mode='HTML')
 
+# @bot.message_handler(content_types=['sticker'])
+# def video(message):
+#     print(message.sticker.file_id)
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     if check(message):
         if message.text != '/start':
             text = (message.text).replace(' ','-')
             # print(text)
-            sahifa = f"https://www.olx.uz/oz/list/q-{text}/"
-            r = requests.get(sahifa)
-            soup = BeautifulSoup(r.text, 'html.parser')
-            news = soup.select('tr[class="wrap"]') # mahsulotlar
-            print(len(news))
-            bot.send_message(message.chat.id,'🕔')
-            filtering_products(news,message)
+            
+            bot.send_message(message.chat.id,'🔎')
+            filtering_products(text,message.chat.id,message.message_id)
         else:
             save_users(message)
             markup = types.InlineKeyboardMarkup(row_width=1)
@@ -51,9 +52,16 @@ def echo_all(message):
             bot.send_message(message.chat.id, 'Assalomu alaykum. Olx ning norasmiy botiga xush kelibsiz.\nBu botda siz hech qanday qiyinchiliklarsiz olx dan bir qancha mahsulotlarni topishingiz mumkin.\n<a href="https://www.olx.uz/oz/">Web Sayt</a>', parse_mode='HTML', reply_markup=markup)
     
 
-def filtering_products(news,message):
-    for j, i in enumerate(news):
-        
+def filtering_products(text,id,message_id):
+    # for j, i in enumerate(news):
+        bot.delete_message(id,message_id)
+        sahifa = f"https://www.olx.uz/oz/list/q-{text}/"
+        r = requests.get(sahifa)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        news = soup.select('tr[class="wrap"]')
+        jami = len(news)
+        tavakkal = random.randrange(0,jami)
+        i = news[tavakkal]
         try:
             price = i.select('p[class="price"] strong')[0].text
         except:
@@ -79,17 +87,18 @@ def filtering_products(news,message):
             get_link = i.select('img[class="fleft"]')[0].parent['href']
         except:
             get_link = None
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        itembtnv = types.InlineKeyboardButton('Boshqasini tanlash', callback_data = text)
+        markup.add(itembtnv)
         
-        id = None
-        # print(numbers[str(j)[0]])
-        if len(str(j)) ==3:
-            id = f"{numbers[str(j)[0]]} {numbers[str(j)[1]]} {numbers[str(j)[2]]}"
-        elif len(str(j)) == 2:
-            id = f"{numbers[str(j)[0]]}{numbers[str(j)[1]]}"
-        else:
-            id = f"{numbers[str(j)[0]]}"
+        # id = None
+        # if len(str(j)) ==3:
+        #     id = f"{numbers[str(j)[0]]} {numbers[str(j)[1]]} {numbers[str(j)[2]]}"
+        # elif len(str(j)) == 2:
+        #     id = f"{numbers[str(j)[0]]}{numbers[str(j)[1]]}"
+        # else:
+        #     id = f"{numbers[str(j)[0]]}"
         text = f'''
-{id}
 
 ✅{title}
 
@@ -100,21 +109,19 @@ def filtering_products(news,message):
 🌐 <a href="{get_link}">Web Sayt</a>
 
         '''
-        # bot.send_message(message.chat.id, text)
-        markup = types.InlineKeyboardMarkup(row_width=2,)
-        itembtna = types.InlineKeyboardButton('Tavsif', callback_data=str(get_link))
-        phone = types.InlineKeyboardButton('📞 Web', callback_data=str(get_link))
-        markup.add(itembtna, phone)
-        bot.send_photo(message.chat.id, pic, caption=text, parse_mode='HTML')
+        bot.send_photo(id, pic, caption=text, parse_mode='HTML', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    subscribe = bot.get_chat_member(chat_id=-1001373178208, user_id=call.from_user.id).status
-    if subscribe == 'creator' or subscribe == 'member' or subscribe == 'administrator':
-        bot.send_message(call.from_user.id, "✅Botimizdan bemalol foydalanishingiz mumkin!\n🔍Foydalanish uchun so'z yuboring.",)
+    if call.data == "check":
+        subscribe = bot.get_chat_member(chat_id=-1001373178208, user_id=call.from_user.id).status
+        if subscribe == 'creator' or subscribe == 'member' or subscribe == 'administrator':
+            bot.send_message(call.from_user.id, "✅ Botimizdan bemalol foydalanishingiz mumkin!\n🔍 Foydalanish uchun so'z yuboring.",)
+        else:
+            bot.send_message(call.from_user.id, "👉Iltimos. kanalga a'zo bo'ling\n@python_flutter_uz")
     else:
-        bot.send_message(call.from_user.id, "👉Iltimos. kanalga a'zo bo'ling\n@python_flutter_uz")   
+        filtering_products(call.data,call.from_user.id,call.message.id)   
 
 def add_recline(message):
     if message.from_user.username == 'shavkatNor' and message.caption:
@@ -131,6 +138,8 @@ def add_recline(message):
 def video(message):
     if message.from_user.username == 'shavkatNor' and message.caption:
         add_recline(message)
+
+
 
 @bot.message_handler(content_types=['photo'])
 def img(message):
